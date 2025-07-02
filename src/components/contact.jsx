178ad32f8048fbeb1b05d7ff73cf8edd1db521fc
sathwik pentakoti <sathwikpentakoti@gmail.com>
2025-07-02 22:58:53 +0530

    small

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'; // ✅ correct import
+import { faCode, faMailBulk, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'; // ✅ correct import
 import myPhoto from '../assets/images/my-photo2.jpg'; // Import your photo
 
 const Contact = () => {
@@ -21,14 +21,11 @@ const Contact = () => {
             <h4>Sathwik</h4>
             <p>Web Developer | Passionate about coding</p>
             <div>
-              <a href="https://www.facebook.com/your-profile" target="_blank" rel="noopener noreferrer" className="mx-2">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-              <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="mx-2">
+              <a href="https://www.linkedin.com/in/sathwik-pentakoti-56868a292" target="_blank" rel="noopener noreferrer" className="mx-2">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
               </a>
-              <a href="https://www.instagram.com/your-profile" target="_blank" rel="noopener noreferrer" className="mx-2">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              <a href="https://leetcode.com/Sathwik_Pentakoti/" target="_blank" rel="noopener noreferrer" className="mx-2">
+                <FontAwesomeIcon icon={faCode} size="2x" />
               </a>
               <a href="https://github.com/sathwik-45" target="_blank" rel="noopener noreferrer" className="mx-2">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
